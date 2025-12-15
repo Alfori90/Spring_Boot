04a610830a9f0a044a374678aad9ed048de6aadc
Huria Alfori <alforihuria938@gmail.com>
2025-12-15 15:29:08 +0400

    Creating new entities

---
+++ Creating new entities/SpringBoot/src/test/java/com/codeline/SpringBoot/Controller/StudentControllerTest.java
@@ -0,0 +1,13 @@
+package com.codeline.SpringBoot.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void createStudent() {
+
+    }
+}
